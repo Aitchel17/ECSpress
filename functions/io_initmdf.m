b67023d266d9b7f58e88957842bed9fb4dbcd176
hql5715 <hql5715@E5-ESM-11313.AD.PSU.EDU>
2024-11-07 18:08:42 -0500

    dft correction drift table part complete, analog channel io in progress

--- functions/io_initmdf.m
+++ functions/io_initmdf.m
@@ -12,9 +12,9 @@
 % WRITTEN BY:       C. Hyunseok Lee 2024-09-14
 %
 %%%%%%%%%%%%%%%%%%%%%%%%
-function [info, tpsm_info, mode_info, mobj] = io_initmdf()
+function [info, mobj] = io_initmdf()
     %%
-    [info.mdfName, info.mdfPath] = uigetfile({'*.tif ; *.mdf'}); % select file by UI
+    [info.mdfName, info.mdfPath] = uigetfile({'*.mdf'}); % select file by UI
     mdfPath = [info.mdfPath, info.mdfName];
     mobj = actxserver('MCSX.Data'); % Create Component Object Model (COM)
     mobj.invoke('OpenMCSFile', mdfPath); % Using COM open .mdf file
@@ -24,31 +24,33 @@ function [info, tpsm_info, mode_info, mobj] = io_initmdf()
     info.Date = mobj.ReadParameter('Created on');
     
     % two photon scanning microscope info
-    tpsm_info.scanmode      = mobj.ReadParameter('Scan Mode');
-    tpsm_info.pclock        = mobj.ReadParameter('Pixel Clock');
-    tpsm_info.yoffset       = mobj.ReadParameter('Y Frame Offset');
+    info.scanmode      = mobj.ReadParameter('Scan Mode');
+    info.pclock        = mobj.ReadParameter('Pixel Clock');
+    info.yoffset       = mobj.ReadParameter('Y Frame Offset');
     
     % Objective info
-    tpsm_info.objname       = mobj.ReadParameter('Objective');
-    tpsm_info.objpix        = mobj.ReadParameter('Microns per Pixel');
-    tpsm_info.objx          = mobj.ReadParameter('X Position');
-    tpsm_info.objy          = mobj.ReadParameter('Y Position');
-    tpsm_info.objz          = mobj.ReadParameter('Z Position');
+    info.objname       = mobj.ReadParameter('Objective');
+    info.objpix        = mobj.ReadParameter('Microns per Pixel');
+    info.objx          = mobj.ReadParameter('X Position');
+    info.objy          = mobj.ReadParameter('Y Position');
+    info.objz          = mobj.ReadParameter('Z Position');
     
     % Scan info
-    tpsm_info.zoom          = mobj.ReadParameter('Magnification');
-    tpsm_info.tpex          = strcat(mobj.ReadParameter('Laser Wavelength (nm)'),' nm');
-    tpsm_info.fbit          = mobj.ReadParameter('Frame Bit Depth');
-    tpsm_info.fdur          = mobj.ReadParameter('Frame Duration (s)');
-    tpsm_info.fcount        = str2double(mobj.ReadParameter('Frame Count'));
-    tpsm_info.fint          = mobj.ReadParameter('Frame Interval (ms)');
-    tpsm_info.fps           = 1/str2double(tpsm_info.fdur(1:end-1)); % Hz
-    tpsm_info.fh            = str2double(mobj.ReadParameter('Frame Height'));
-    tpsm_info.fw            = str2double(mobj.ReadParameter('Frame Width'));
-    tpsm_info.lpower        = mobj.ReadParameter('Laser intensity');
+    info.zoom          = mobj.ReadParameter('Magnification');
+    info.tpex          = strcat(mobj.ReadParameter('Laser Wavelength (nm)'),' nm');
+    info.fbit          = mobj.ReadParameter('Frame Bit Depth');
+    info.fdur          = mobj.ReadParameter('Frame Duration (s)');
+    info.fcount        = str2double(mobj.ReadParameter('Frame Count'));
+    info.fint          = mobj.ReadParameter('Frame Interval (ms)');
+    info.fps           = 1/str2double(info.fdur(1:end-1)); % Hz
+    info.fh            = str2double(mobj.ReadParameter('Frame Height'));
+    info.fw            = str2double(mobj.ReadParameter('Frame Width'));
+    info.lpower        = mobj.ReadParameter('Laser intensity');
     % Imaging Channel info
-    tpsm_info.imgch0range   = mobj.ReadParameter('Scanning Ch 0 Input Range');
-    tpsm_info.imgch1range   = mobj.ReadParameter('Scanning Ch 1 Input Range');
+    info.imgch0name    = mobj.ReadParameter('Scanning Ch 0 Name');
+    info.imgch0range   = mobj.ReadParameter('Scanning Ch 0 Input Range');
+    info.imgch1name    = mobj.ReadParameter('Scanning Ch 1 Name');
+    info.imgch1range   = mobj.ReadParameter('Scanning Ch 1 Input Range');
 
 
 
@@ -57,30 +59,40 @@ function [info, tpsm_info, mode_info, mobj] = io_initmdf()
 
 
 %% Scan mode specific info
-    if strcmp(tpsm_info.scanmode, 'Image Stack')
+    if strcmp(info.scanmode, 'Image Stack')
         disp('Image stack loaded')
-        mode_info.fave      = mobj.ReadParameter('Average Count');
-        mode_info.pinit     = mobj.ReadParameter('Initial Intensity');
+        info.fave      = mobj.ReadParameter('Average Count');
+        info.pinit     = mobj.ReadParameter('Initial Intensity');
         % mode_info.pfinl     = mobj.ReadParameter('Final Intensity');
         % final intensity activex control has bug the .ocx file should be
         % editted
-        mode_info.zinter    = mobj.ReadParameter('Z- interval');
+        info.zinter    = mobj.ReadParameter('Z- interval');
         
-    elseif strcmp(tpsm_info.scanmode, 'XY Movie')
-        mode_info.analogfreq    = mobj.ReadParameter('Analog Acquisition Frequency (Hz)');
-        mode_info.analogfreq    = str2double(mode_info.analogfreq(1:end -3));
-        mode_info.analogcount   = str2double(mobj.ReadParameter('Analog Sample Count'));
-        mode_info.analogresolution   = mobj.ReadParameter('Analog Resolution');
-
-        mode_info.analog0   = mobj.ReadParameter('Analog Ch 0 Name');
-        mode_info.analog1   = mobj.ReadParameter('Analog Ch 1 Name');
-        mode_info.analog2   = mobj.ReadParameter('Analog Ch 2 Name');
-        mode_info.analog3   = mobj.ReadParameter('Analog Ch 3 Name');
-        mode_info.analog4   = mobj.ReadParameter('Analog Ch 4 Name');
-        mode_info.analogch4range = mobj.ReadParameter('Analog Ch 4 Input Range');
-        mode_info.analog5   = mobj.ReadParameter('Analog Ch 5 Name');
-        mode_info.analog6   = mobj.ReadParameter('Analog Ch 6 Name');
-        mode_info.analog7   = mobj.ReadParameter('Analog Ch 7 Name');
+    elseif strcmp(info.scanmode, 'XY Movie')
+        info.analogfreq    = mobj.ReadParameter('Analog Acquisition Frequency (Hz)');
+        info.analogfreq    = str2double(info.analogfreq(1:end -3));
+        info.analogcount   = str2double(mobj.ReadParameter('Analog Sample Count'));
+        info.analogresolution   = mobj.ReadParameter('Analog Resolution');
+        info.analog0   = mobj.ReadParameter('Analog Ch 0 Name');
+        info.analog1   = mobj.ReadParameter('Analog Ch 1 Name');
+        info.analog2   = mobj.ReadParameter('Analog Ch 2 Name');
+        info.analog3   = mobj.ReadParameter('Analog Ch 3 Name');
+        info.analog4   = mobj.ReadParameter('Analog Ch 4 Name');
+        info.analog5   = mobj.ReadParameter('Analog Ch 5 Name');
+        info.analog6   = mobj.ReadParameter('Analog Ch 6 Name');
+        info.analog7   = mobj.ReadParameter('Analog Ch 7 Name');
+        for analog_ch = string(0:1:7)
+            field_name = strcat('analog',analog_ch);
+            if strcmp(info.(field_name),"")
+                info = rmfield(info,field_name);
+            else
+                range_fname = strcat('analog',analog_ch,'range');
+                range_call = strcat('Analog ', 'ch ',analog_ch,' Input Range');
+                info.(range_fname) = mobj.ReadParameter(range_call);
+            end
+        end
     end
+
+
 
 end
--- functions/pre_groupaverage.m
+++ functions/pre_groupaverage.m
@@ -1,16 +1,12 @@
-function [outputArg1,outputArg2] = pre_groupaverage(zstack, number_averaging)
+function [result] = pre_groupaverage(zstack, number_averaging)
 %FRAME_AVERAGING Summary of this function goes here
 %   Detailed explanation goes here
 
+
+zstack = zstack(:,:,1:size(zstack,3)-mod(size(zstack,3),number_averaging));
+
 result = squeeze( ...
     mean( ...
-    reshape(zstack,size(data.prestack,1),size(tmp.medstack,2),number_averaging,[]),3) ... % reshape by 
+    reshape(zstack,size(zstack,1),size(zstack,2),number_averaging,[]),3) ... % reshape by 
        ... % calculate mean by axis
     ); ... % Remove averaging axis
-figure()
-sliceViewer(tmp.gpave_stack)
-
-figure()
-imshow(mean(data.prestack,3), [0, 1000])
-end
-
