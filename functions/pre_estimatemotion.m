30fef96c29a623f66763c21ec796a93d72e26d9d
hql5715 <hql5715@E5-ESM-11313.AD.PSU.EDU>
2024-11-24 19:04:20 -0500

    ecspress: from image loading to motion correction, apply drifttable to the other channel pre_setimatemotion: calculate motion and generate drift table pre_applymotion: apply drifttable made by pre_estimatemotion savetiff: have problem of drifting image when imagedescription tag is added, perhaps stripoffsets tag to be adjusted but not changable

---
+++ functions/pre_estimatemotion.m
@@ -0,0 +1,53 @@
+function [pixelShift_table] = pre_estimatemotion(dst_stack)
+    figure(77)
+    % display the first frame of the .tif stack so the user can draw a box on
+    % steady region (not contain artery)
+    sliceViewer(dst_stack);
+    title('draw a box around region to be processed')
+    thebox=drawrectangle(gca);
+    xy=round(thebox.Vertices);
+    input('type enter when done','s');
+    close(77)
+    raw_hold_image=double(dst_stack);
+    hold_stack=raw_hold_image(xy(1,2):xy(3,2), xy(1,1):xy(3,1),:);
+    
+    figure(5)
+    sliceViewer(hold_stack);
+    
+    first_fft = fft2(hold_stack(:,:,end));
+    
+    pixelShift_table = [];
+    for sli = drange(1:size(hold_stack,3))
+        disp(sli)
+        regframe = fft2(hold_stack(:,:,sli));
+        [pixelShift_table(:,sli),~] = dft_registration(first_fft,regframe);
+% Inputs
+% buf1ft    Fourier transform of reference image, 
+%           DC in (1,1)   [DO NOT FFTSHIFT]
+% buf2ft    Fourier transform of image to register, 
+%           DC in (1,1) [DO NOT FFTSHIFT]
+% usfac     Upsampling factor (integer). Images will be registered to 
+%           within 1/usfac of a pixel. For example usfac = 20 means the
+%           images will be registered within 1/20 of a pixel. (default = 1)
+%
+% Outputs
+% output =  [error,diffphase,net_y_shift,net_x_shift]
+% error     Translation invariant normalized RMS error between f and g
+% diffphase     Global phase difference between the two images (should be
+%               zero if images are non-negative).
+% net_row_shift net_col_shift   Pixel shifts between images
+% Greg      (Optional) Fourier transform of registered version of buf2ft,
+%           the global phase difference is compensated for.
+    end
+
+    figure('name', 'pixel shift','NumberTitle','off')
+    subplot(2,1,1)
+    xshift = pixelShift_table(4,:);
+    plot(xshift)
+    title('X shift')
+    subplot(2,1,2)
+    yshift = pixelShift_table(3,:);
+    plot(yshift)
+    title('Y-shift')
+end
+
