fe290e8e3c69850e4b1fabdb6f7ac06de272fec2
hql5715 <hql5715@E5-ESM-11313.AD.PSU.EDU>
2024-12-15 15:53:26 -0500

    Use median rather than mean GPU acceleration

--- functions/pre_thresholding.m
+++ functions/pre_thresholding.m
@@ -2,9 +2,11 @@ function [thrstack] = pre_thresholding(inputstack)
 %PRE_RESCALE_UINT16 Summary of this function goes here
 %   Detailed explanation goes here
     % rescale
+    inputstack = gpuArray(inputstack);
     [height, width, ~] = size(inputstack);
-    threshold = min(mean(inputstack(1+ceil(height/3):end-ceil(height/3),1+ceil(width/3):end-ceil(width/3),:),3),[],'all');
+    threshold = min(median(inputstack(1+ceil(height/3):end-ceil(height/3),1+ceil(width/3):end-ceil(width/3),:),3),[],'all');
     thrstack = inputstack-threshold;
     thrstack(thrstack<0) = 0;
+    thrstack = gather(thrstack);
 end
 
