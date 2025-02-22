8fd84aba30811158affb82eb40246a2d8e2ef6a2
Aitchel17 <lhs4869@gmail.com>
2025-02-22 17:58:31 -0500

    subclass of roi, designed to analyze bleaching experiment, and perivascular space analysis

---
+++ class/bleach.m
@@ -0,0 +1,31 @@
+classdef bleach < roi
+    %BLEACH Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        
+    end
+    
+    methods
+        function obj = bleach(stackname,roitype)
+           obj@roi(stackname,roitype);
+        end
+        
+        function data = decay(obj,stackfieldname)
+            data = obj.data;
+            % field name construction
+            fname_ave = ['ave_', stackfieldname];
+            fname_norm = ['norm_', stackfieldname];
+            fname_exponential1 = ['exponential1_', stackfieldname];
+            fname_exponential2 = ['exponential2_', stackfieldname];
+
+            data.(fname_ave) = squeeze(mean(obj.stacks.(stackfieldname),[1,2],'omitmissing'));
+            data.(fname_norm) = data.(fname_ave)/data.(fname_ave)(1);
+            data.(fname_exponential1) = log(data.(fname_ave));
+            data.(fname_exponential1) = data.(fname_exponential1)/data.(fname_exponential1)(1);
+            data.(fname_exponential2) = log(log(data.(fname_ave)));
+            data.(fname_exponential2) = data.(fname_exponential2)/data.(fname_exponential2)(1);
+        end
+    end
+end
+
