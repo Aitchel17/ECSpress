505a24393c283fb3574b5468d3b1c95cfea65b64
Aitchel17 <lhs4869@gmail.com>
2025-07-28 19:12:44 -0400

    class update for fwhm analysis

---
+++ class/color_lee.m
@@ -0,0 +1,30 @@
+classdef color_lee
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        red_g = []
+        green_g = []
+        blue_g = []
+        magenta_g = []
+        cyan_g = []
+        yellow_g = []
+        gray_g = []
+    end
+    
+    methods
+        function obj = color_lee()
+            %UNTITLED Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.red_g =    [linspace(0,1,256)', zeros(256,1), zeros(256,1)];
+            obj.green_g =    [zeros(256,1), linspace(0,1,256)', zeros(256,1)];
+            obj.blue_g =    [zeros(256,1), zeros(256,1), linspace(0,1,256)'];
+            obj.magenta_g =    [linspace(0,1,256)', zeros(256,1), linspace(0,1,256)'];
+            obj.cyan_g =    [zeros(256,1),linspace(0,1,256)', linspace(0,1,256)'];
+            obj.yellow_g =    [linspace(0,1,256)', linspace(0,1,256)',zeros(256,1)];
+            obj.gray_g =    [linspace(0,1,256)', linspace(0,1,256)', linspace(0,1,256)'];
+
+        end
+    end
+end
+
